6e73794412d3a24a7e50ceda13fc0ac4185d7f10
Ken Urata <senseiurata@gmail.com>
2015-09-26 19:25:54 -0700

    Replaces dummy friends data with live data

--- src/friendgroup.js
+++ src/friendgroup.js
@@ -1,103 +1,37 @@
-var groups = [
-    {
-        "id": "1",
-        "name": "Superheros",
-        "friendIds": [
-            "1",
-            "2",
-            "3",
-            "10",
-            "15",
-            "21",
-            "4"
-        ]
-    },
-    {
-        "id": "3",
-        "name": "People",
-        "friendIds": [
-            "9",
-            "21",
-            "1"
-        ]
-    },
-    {
-        "id": "4",
-        "name": "Fictional Characters",
-        "friendIds": [
-            "10",
-            "11",
-            "1"
-        ]
-    }
-];
-
-var friends = [
-    {
-        "id": "1",
-        "name": "Ken"
-    },
-    {
-        "id": "2",
-        "name": "Esther"
-    },
-    {
-        "id": "3",
-        "name": "Michael Scott"
-    },
-    {
-        "id": "4",
-        "name": "Jim Halpert"
-    },
-    {
-        "id": "5",
-        "name": "Pam Beesly"
-    },
-    {
-        "id": "6",
-        "name": "Batman"
-    },
-    {
-        "id": "7",
-        "name": "Superman"
-    },
-    {
-        "id": "8",
-        "name": "Iron Man"
-    },
-    {
-        "id": "9",
-        "name": "The Hulk"
-    },
-    {
-        "id": "10",
-        "name": "Arya Stark"
-    },
-    {
-        "id": "11",
-        "name": "Daenerys Targaryen"
-    },
-    {
-        "id": "12",
-        "name": "Jaime Lannister"
-    },
-    {
-        "id": "13",
-        "name": "Goku"
-    },
-    {
-        "id": "14",
-        "name": "Mario"
-    }
-];
-
 var FriendGroupBox = React.createClass({
+  getInitialState: function () {
+    return {
+      groupsData: [],
+      friendsData: []
+    };
+  },
+  componentDidMount: function () {
+    $.ajax({
+      method: 'get',
+      url: this.props.groupsUrl,
+      success: function (data) {
+        this.setState({
+          groupsData: data
+        });
+      }.bind(this)
+    });
+
+    $.ajax({
+      method: 'get',
+      url: this.props.friendsUrl,
+      success: function (data) {
+        this.setState({
+          friendsData: data
+        });
+      }.bind(this)
+    });
+  },
   render: function () {
     return (
       <div>
         <h2>Friend Group</h2>
 
-        <FriendGroupList groupsData={this.props.groupsData} friendsData={this.props.friendsData} />
+        <FriendGroupList groupsData={this.state.groupsData} friendsData={this.state.friendsData} />
       </div>
     );
   }
@@ -162,6 +96,6 @@ var Friend = React.createClass({
 });
 
 React.render(
-  <FriendGroupBox groupsData={groups} friendsData={friends} />,
+  <FriendGroupBox groupsUrl={"/groups"} friendsUrl={"/friends"} />,
   document.getElementById('friend-group')
 );
